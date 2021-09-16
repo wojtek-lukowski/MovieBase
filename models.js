5e8c9a3c6521f44d898eda15e6644853077940a5
Wojtek Lukowski <wlukowski@4qu.de>
2021-09-16 23:37:12 +0200

    Mongo connected, errors

---
+++ models.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+Title: {type: String, required: true},
+Description: {type: String, required: true},
+Genre: {
+    Name: String,
+    Description: String
+},
+Director: {
+    Name: String,
+    Bio: String
+},
+Actors: [String],
+ImagePath: String,
+Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    Favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let actorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String},
+    Birthday: Date,
+    Movies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movies'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+let Actor = mongoose.model('Actor', actorSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Actor = Actor;
